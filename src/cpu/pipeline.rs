5bb47e1373d9d2397903bdaad61c6b555281c5c5
felixzhuologist <felix.czhu@gmail.com>
2019-07-09 18:15:52 -0400

    add more exhaustive arm disassembler tests

--- src/cpu/pipeline.rs
+++ src/cpu/pipeline.rs
@@ -1,3 +1,4 @@
+//! Utilities for emulating the CPU's instruction pipeline
 use self::Instruction::{
     DataProc,
     PSRTransfer,
@@ -39,10 +40,16 @@ pub enum PipelineInstruction {
     RawARM(u32),
     /// A fetched THUMB instruction
     RawTHUMB(u16),
-    /// A decoded instruction
+    // TODO: change the Option<u32> to an Option<CondField> instead since we
+    // don't need the rest of the bits
+    /// A decoded instruction, containing both the original raw instruction
+    /// as well as the parsed Instruction
     Decoded(Option<u32>, Instruction)
 }
 
+/// Decode a raw ARM instruction
+// NOTE: this will incorrectly parse some undefined instructions, but we assume
+// that games will never run those
 pub fn decode_arm(ins: u32) -> Option<Instruction> {
     let op0 = util::get_nibble(ins, 24);
     let op1 = util::get_nibble(ins, 20);
@@ -61,7 +68,9 @@ pub fn decode_arm(ins: u32) -> Option<Instruction> {
     } else if op0 < 4 {
         let data = data::DataProc::parse_instruction(ins);
         let op = data.opcode as u8;
-        if !data.set_flags && op > 7 && op < 12 {
+        // PSR instructions are Data Processing operations with TST, TEQ, CMP,
+        // or CMN, without the S flag set
+        if !data.set_flags && op >= 8 && op <= 11 {
             Some(PSRTransfer(psr::PSRTransfer::parse_instruction(ins)))
         } else {
             Some(DataProc(data))
@@ -79,6 +88,7 @@ pub fn decode_arm(ins: u32) -> Option<Instruction> {
     }
 }
 
+/// Decode a raw thumb instruction
 pub fn decode_thumb(ins: u16) -> Instruction {
     // this intermediate function exists to be able to test that the correct
     // THUMB format is identified
@@ -132,6 +142,7 @@ fn _decode_thumb(ins: u16) -> (fn(u16) -> Instruction) {
     }
 }
 
+/// The possible instructions of the ARM instruction set
 #[derive(Clone, Debug)]
 pub enum Instruction {
     DataProc(data::DataProc),
@@ -149,7 +160,8 @@ pub enum Instruction {
     LongBranch(thumb::LongBranch),
 }
 
-/// Return whether the current state of the CPU's flags matches the given condition
+/// Return whether the current state of the CPU's flags satisfies the condition
+/// field of the given raw instruction
 pub fn satisfies_cond(cpsr: &PSR, cond: u32) -> bool {
     match CondField::from_u32(cond).unwrap() {
         CondField::EQ => cpsr.zero,
@@ -170,6 +182,9 @@ pub fn satisfies_cond(cpsr: &PSR, cond: u32) -> bool {
     }
 }
 
+/// Each ARM instruction's most significant 4 bits contain a condition field
+/// which is compared with the CPU's CPSR register to determine if the instruction
+/// should be executed
 enum_from_primitive! {
 #[repr(u8)]
 pub enum CondField {
@@ -193,7 +208,6 @@ pub enum CondField {
 
 #[cfg(test)]
 mod test {
-
     mod decode_arm {
         use super::super::*;
 
@@ -201,20 +215,46 @@ mod test {
             ($instr:expr, $instr_type: pat) => (
                 assert!(match decode_arm($instr).unwrap() {
                     $instr_type => true,
-                    _ => false
+                    result => {
+                        println!("got: {:?}", result);
+                        false
+                    }
                 })
             )
         }
 
-        #[test]
-        fn branch() {
-            has_type!(0x0_A_123456, Instruction::Branch(_));
-            has_type!(0x0_B_123456, Instruction::Branch(_));
+        // for decode_arm()'s condition branches that are based exclusively on the op0, op1,
+        // and op2 nibbles (which is all but 2), we can effectively test against all possible options
+        // by enumerating all possible values for op0, op1, and op2. we do this
+        // using generate_instructions(), which will generate all possible values
+        // after possibly fixing any of the 12 bits (specified using a NibbleFilter).
+        // we can then write tests for each instruction by copying fixed values
+        // directly from the instruction table in the ARM manual (Figure 4-1)
+
+        type NibbleFilter = (Option<u32>, Option<u32>, Option<u32>, Option<u32>);
+
+        fn generate_instructions(f0: &NibbleFilter, f1: &NibbleFilter, f2: &NibbleFilter) -> Vec<u32> {
+            let mut instructions = Vec::new();
+            for op0 in generate_nibbles(f0) {
+                for op1 in generate_nibbles(f1) {
+                    for op2 in generate_nibbles(f2) {
+                        instructions.push((op0 << 24) | (op1 << 20) | (op2 << 4))
+                    }
+                }
+            }
+            instructions
         }
 
-        #[test]
-        fn bex() {
-            has_type!(0x0_12FFF1_5, Instruction::BranchEx(_));
+        fn generate_nibbles(filter: &NibbleFilter) -> Vec<u32> {
+            (0u32..16u32).filter(|num| matches_filter(num, filter)).collect()
+        }
+
+        fn matches_filter(num: &u32, filter: &NibbleFilter) -> bool {
+            let (f3, f2, f1, f0) = filter;
+            f3.map_or(true, |val| ((num >> 3) & 1u32) == val) &&
+            f2.map_or(true, |val| ((num >> 2) & 1u32) == val) &&
+            f1.map_or(true, |val| ((num >> 1) & 1u32) == val) &&
+            f0.map_or(true, |val| ((num >> 0) & 1u32) == val)
         }
 
         #[test]
@@ -223,18 +263,6 @@ mod test {
             has_type!(0xA3123456, Instruction::DataProc(_));
             has_type!(0x001A3D56, Instruction::DataProc(_));
             has_type!(0b11100001101000000110100100010110, Instruction::DataProc(_));
-        }
-
-        #[test]
-        fn mul() {
-            has_type!(0x03_123_9_A, Instruction::Multiply(_));
-            has_type!(0x02_ABC_9_0, Instruction::Multiply(_));
-        }
-
-        #[test]
-        fn mul_long() {
-            has_type!(0x08_123_9_A, Instruction::MultiplyLong(_));
-            has_type!(0x0B_ABC_9_0, Instruction::MultiplyLong(_));
         }
 
         #[test]
@@ -248,33 +276,94 @@ mod test {
         }
 
         #[test]
+        fn mul() {
+            let f0 = (Some(0), Some(0), Some(0), Some(0));
+            let f1 = (Some(0), Some(0), None, None);
+            let f2 = (Some(1), Some(0), Some(0), Some(1));
+            for ins in generate_instructions(&f0, &f1, &f2) {
+                has_type!(ins, Instruction::Multiply(_));
+            }
+        }
+
+        #[test]
+        fn mul_long() {
+            let f0 = (Some(0), Some(0), Some(0), Some(0));
+            let f1 = (Some(1), None, None, None);
+            let f2 = (Some(1), Some(0), Some(0), Some(1));
+            for ins in generate_instructions(&f0, &f1, &f2) {
+                has_type!(ins, Instruction::MultiplyLong(_));
+            }
+        }
+
+        #[test]
+        fn single_data_swap() {
+            let f0 = (Some(0), Some(0), Some(0), Some(1));
+            let f1 = (Some(0), None, Some(0), Some(0));
+            let f2 = (Some(1), Some(0), Some(0), Some(1));
+            for ins in generate_instructions(&f0, &f1, &f2) {
+                has_type!(ins, Instruction::SwapTransfer(_));
+            }
+        }
+
+        #[test]
+        fn bex() {
+            has_type!(0x0_12FFF1_5, Instruction::BranchEx(_));
+        }
+
+        #[test]
+        fn signed_halfword_transfer() {
+            let f0 = (Some(0), Some(0), Some(0), None);
+            let f1 = (None, None, None, None);
+            let f2 = (Some(1), None, None, Some(1));
+            for ins in generate_instructions(&f0, &f1, &f2) {
+                let op0 = util::get_nibble(ins, 24);
+                let op1 = util::get_nibble(ins, 20);
+                let op2 = util::get_nibble(ins, 4);
+                // mul instruction takes precedence
+                if op2 == 9 && op0 == 0 && (op1 <= 3 || op1 >= 8) {
+                    continue;
+                }
+                // why ????
+                if op2 == 9 && op0 == 1 {
+                    continue;
+                }
+                has_type!(ins, Instruction::SignedTransfer(_));
+            }
+        }
+
+        #[test]
         fn single_trans() {
-            has_type!(0xA_4_123456, Instruction::SingleTransfer(_));
-            has_type!(0xA_7_ABCDEF, Instruction::SingleTransfer(_));
+            let f0 = (Some(0), Some(1), None, None);
+            let f1 = (None, None, None, None);
+            let f2 = (None, None, None, None);
+            for ins in generate_instructions(&f0, &f1, &f2) {
+                has_type!(ins, Instruction::SingleTransfer(_));
+            }
         }
 
         #[test]
         fn block_trans() {
-            has_type!(0x0_8_123456, Instruction::BlockTransfer(_));
-            has_type!(0x0_9_1DFA10, Instruction::BlockTransfer(_));
+            let f0 = (Some(1), Some(0), Some(0), None);
+            let f1 = (None, None, None, None);
+            let f2 = (None, None, None, None);
+            for ins in generate_instructions(&f0, &f1, &f2) {
+                has_type!(ins, Instruction::BlockTransfer(_));
+            }
+        }
+
+        #[test]
+        fn branch() {
+            let f0 = (Some(1), Some(0), Some(1), None);
+            let f1 = (None, None, None, None);
+            let f2 = (None, None, None, None);
+            for ins in generate_instructions(&f0, &f1, &f2) {
+                has_type!(ins, Instruction::Branch(_));
+            }
         }
 
         #[test]
         fn sw_interrupt() {
             has_type!(0xFF_123ABC, Instruction::SWInterrupt(_));
-        }
-
-        #[test]
-        fn swap() {
-            has_type!(0xF_1_0_123_9_5, Instruction::SwapTransfer(_));
-            has_type!(0xF_1_8_ABC_9_E, Instruction::SwapTransfer(_));
-        }
-
-        #[test]
-        fn signed_halfword_transfer() {
-            has_type!(0xF_1_0BE0_B_3, Instruction::SignedTransfer(_));
-            has_type!(0xF_0_FABC_D_3, Instruction::SignedTransfer(_));
-            has_type!(0xF_0_7123_F_3, Instruction::SignedTransfer(_));
         }
     }
 
